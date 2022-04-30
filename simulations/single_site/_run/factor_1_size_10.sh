15be3c5450cf499c59bb0e32c1346e1d0ab3f6d3
Mike <mikeryanmay@gmail.com>
2022-04-30 12:36:42 -0700

    hmm

--- simulations/single_site/_run/factor_1_size_10.sh
+++ simulations/single_site/_run/factor_1_size_10.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-user=mikeryanmay@gmail.edu
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --ntasks=24
-#SBATCH --time=24:00:00
+#SBATCH --ntasks=20
+#SBATCH -t 24:00:00
 
 # change to user directory
 cd /home/$USER/YuleSelectionSims/simulations/single_site/factor_1_size_10/
