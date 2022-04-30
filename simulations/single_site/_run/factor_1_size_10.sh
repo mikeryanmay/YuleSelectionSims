b217e3d0c9cd05cc7f07dd4fb9acbf3041d0d4d3
Mike <mikeryanmay@gmail.com>
2022-04-30 12:43:02 -0700

    tweaks

--- simulations/single_site/_run/factor_1_size_10.sh
+++ simulations/single_site/_run/factor_1_size_10.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=low2
+#SBATCH --partition=med2
 #SBATCH --account=brannalagrp
 #SBATCH --job-name=factor_1_size_10
 #SBATCH --mail-user=mikeryanmay@gmail.edu
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --ntasks=20
-#SBATCH -t 00:01:00
+#SBATCH --ntasks=24
+#SBATCH --time=72:00:00
 
 # change to user directory
 cd /home/$USER/YuleSelectionSims/simulations/single_site/factor_1_size_10/
@@ -14,12 +14,5 @@ cd /home/$USER/YuleSelectionSims/simulations/single_site/factor_1_size_10/
 # load the module
 module load R
 
-# make the output directory
-mkdir -p logs
-
 # run your code
-parallel -j $SLURM_CPUS_ON_NODE "Rscript ../../../src/analysis.R {%} > 'logs/rep_{}.txt'" ::: {1..100}
-
-# move log file
-mkdir -p log
-mv "slurm-${SLURM_JOB_ID}.out" "log/slurm-${SLURM_JOB_ID}.out"
+parallel -j $SLURM_CPUS_ON_NODE "Rscript ../../../src/analysis.R {%}" ::: {1..100}
